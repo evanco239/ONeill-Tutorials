376b14634be8b669dc8684ecf808618e19b7adda
evanco239 <evanco239@gmail.com>
2019-05-13 11:44:53 -0400

    Updating Tutorials
    Need links to source files for anyone watching

--- Star Wars API/main.js
+++ Star Wars API/main.js
@@ -1,15 +1,88 @@
-// people, planets, films, species, starships, vehicles
+const results = document.querySelector("#results");
 
-async function asyncFetch() {
-    const res = await fetch("https://swapi.co/api/");
-    const data = await res.json();
-    console.log('async data ', data);
+async function asyncFetch(value) {
+  const res = await fetch(`https://swapi.co/api/${value ? value + "/" : ""}`);
+  const data = await res.json();
+  displayResults(data, value);
 }
-  
 
-asyncFetch()
+function displayResults(data, value) {
+  let output = "";
+  // films
+  if (value === "films") {
+    data.results.forEach(item => {
+      console.log(item);
+      output += `
+            <div class="card p-3 m-3" style="opacity:.8">
+                <h4 class="card-title text-center">${item.title}</h4>
+                <div class="card-content">
+                    <span style="text-decoration:underline">Producer</span>: ${
+                      item.producer
+                    }<br>
+                    <span style="text-decoration:underline">Director</span>: ${
+                      item.director
+                    }<p>
+                    <span style="text-decoration:underline">Release Date</span>: ${
+                      item.release_date
+                    }<p>
+                    <p class="text-center">${item.opening_crawl}</p>
+                </div>
+            </div>
+        `;
+      results.innerHTML = output;
+    });
+  }
+  // people
+  if (value === "people") {
+    data.results.forEach(item => {
+      console.log(item);
+      output += `
+            <div class="card p-3 m-3" style="opacity:.8">
+                <h4 class="card-title text-center">${item.name}</h4>
+                <div class="card-content">
+                    <span style="text-decoration:underline">Height</span>: ${
+                      item.height
+                    }<br>
+                    <span style="text-decoration:underline">Birth Year</span>: ${
+                      item.birth_year
+                    }<p>
+                    <span style="text-decoration:underline">Skin Color</span>: ${
+                      item.skin_color
+                    }
+                </div>
+            </div>
+        `;
+      results.innerHTML = output;
+    });
+  }
+  // vehicles
+  if (value === "vehicles") {
+    data.results.forEach(item => {
+      console.log(item);
+      output += `
+            <div class="card p-3 m-3" style="opacity:.8">
+                <h4 class="card-title text-center">${item.name}</h4>
+                <div class="card-content">
+                    <span style="text-decoration:underline">Capacity</span>: ${
+                      item.cargo_capacity
+                    }<br>
+                    <span style="text-decoration:underline">Model</span>: ${
+                      item.model
+                    }<br>
+                    <span style="text-decoration:underline">Manufacturer</span>: ${
+                      item.manufacturer
+                    }<br>
+                    <span style="text-decoration:underline">Vehicle Class</span>: ${
+                      item.vehicle_class
+                    }<br>
+                </div>
+            </div>
+        `;
+      results.innerHTML = output;
+    });
+  }
+}
 
-document.querySelector('input').addEventListener('click', (e) => {
-    console.log(e);
-    console.log(document.querySelector('#buttons').id);
-})
+document.querySelector("#buttons").addEventListener("click", e => {
+  asyncFetch(e.target.textContent.trim().toLowerCase());
+});
